36d8aaa2733078b044ce168e56cf02fef284b6f4
jojii <jojii@gmx.net>
2019-09-05 21:12:10 +0200

    updated installer

--- main/install.sh
+++ main/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Be sure you have installed go >= 1.12 if not, press Ctrl+c"
-sleep 5
+sleep 4
 go get
 go build
 ./main
 chmod 600 cfg.json data.db
+mkdir ./temp
+chmod 770 ./temp -R
